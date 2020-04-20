03a727d5465e64e75a797d7b8583edc6af7e6a8d
Ankit Anubhav <31243315+ankitanubhav212@users.noreply.github.com>
2020-04-20 17:29:13 +0530

    Update header.js

--- src/shared/header.js
+++ src/shared/header.js
@@ -5,7 +5,7 @@ export function Header() {
     <nav className="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
       <div className="container">
         <div className="navbar-header">
-          <a className="navbar-brand" href="#">Restaurant</a>
+          <a className="navbar-brand" href="#">Restaurants</a>
         </div>
       </div>
     </nav>
